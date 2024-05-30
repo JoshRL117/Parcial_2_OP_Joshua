5ed1b80cb9241e757762112df1bf9539edbb317f
JoshRL117 <100550089+JoshRL117@users.noreply.github.com>
2024-05-30 08:28:00 -0600

    Faltaba parte del proceso

--- busqueda_unidireccional.py
+++ busqueda_unidireccional.py
@@ -18,12 +18,12 @@ class optimizador:
     def primeraderivadanumerica(self, x_actual,f):
         delta=0.0001
         numerador= f(x_actual + delta) - f (x_actual - delta) 
-        return numerador / (2 * delta)
+        return (numerador / (2 * delta))
 
     def segundaderivadanumerica(self, x_actual,f):
         delta=0.0001
         numerado= f(x_actual + delta) - (2 * f (x_actual + f(x_actual- delta))) 
-        return numerado / (delta**2)
+        return (numerado / (delta**2))
 
     def newton_raphson(self,x,e,f):
         cont=0
@@ -34,13 +34,13 @@ class optimizador:
         print("{} - {} / {}".format(x_actual,xderiv,xderiv2))
         print(xsig)
         x_actual=xsig
-        while  cont < 5:
-            print(" {} > {}".format(self.primeraderivadanumerica(xsig,f),e))
+        while  (abs(self.primeraderivadanumerica(xsig,f)) > e):
+            #print(" {} > {}".format(self.primeraderivadanumerica(xsig,f),e))
             xderiv=self.primeraderivadanumerica(x_actual,f)
             xderiv2=self.segundaderivadanumerica(x_actual,f)
-            print("{} - {} / {}".format(x_actual,xderiv,xderiv2))
-            print(xsig)
+            #print(xsig)
             xsig= x_actual  - (xderiv/xderiv2)
+            #print("{} - {} / {}".format(x_actual,xderiv,xderiv2))
             x_actual=xsig
             #print("{} - {}".format(x_actual,(xderiv/xderiv2) ))
             cont+=1
@@ -49,7 +49,7 @@ class optimizador:
     
     def evaluar_alpha(self,alpha):
         x_alpha=(self.x_t) + (alpha * self.x_s)
-        x_nuevo=self.funcion(x_alpha[0],x_alpha[1])
+        x_nuevo=self.funcion(x_alpha[0],x_alpha[1])#Se evalua el punto
         return x_nuevo
     
     def busqueda_unidireccional(self,epsilon):
@@ -61,7 +61,7 @@ class optimizador:
 
 x_t=np.array([1,1])
 x_s=np.array([1,0.5])
-alpha=0.5
+alpha=0.2
 
 epsilon=0.1
 optimi=optimizador(x_t,x_s,himmelblau,alpha)
