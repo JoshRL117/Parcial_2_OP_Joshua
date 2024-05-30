d9b1720b7c6e8dba4b65577238644a4a7f06906d
JoshRL117 <100550089+JoshRL117@users.noreply.github.com>
2024-05-30 07:52:35 -0600

    Update busqueda_unidireccional.py

--- busqueda_unidireccional.py
+++ busqueda_unidireccional.py
@@ -4,6 +4,9 @@ def funcionprueba(x1,x2):
 
 def funcion1(x1,x2):
     return ((x1- 10)**2 ) + ((x2 - 10)**2)
+
+def himmelblau(p1,p2):
+    return (p1**2 + p2 - 11)**2 + (p1 + p2**2 - 7)**2
 #Busqueda unidireccional 
 class optimizador: 
     def __init__(self,x_t,x_s,funcion,alpha):
@@ -23,18 +26,24 @@ class optimizador:
         return numerado / (delta**2)
 
     def newton_raphson(self,x,e,f):
+        cont=0
         x_actual=x
         xderiv=self.primeraderivadanumerica(x_actual,f)
         xderiv2=self.segundaderivadanumerica(x_actual,f)
         xsig= x_actual  - (xderiv/xderiv2)
+        print("{} - {} / {}".format(x_actual,xderiv,xderiv2))
+        print(xsig)
         x_actual=xsig
-        while (self.primeraderivadanumerica(xsig,f) > e):
+        while  cont < 5:
             print(" {} > {}".format(self.primeraderivadanumerica(xsig,f),e))
             xderiv=self.primeraderivadanumerica(x_actual,f)
             xderiv2=self.segundaderivadanumerica(x_actual,f)
-            xsig= x_actual  + (xderiv/xderiv2)
+            print("{} - {} / {}".format(x_actual,xderiv,xderiv2))
+            print(xsig)
+            xsig= x_actual  - (xderiv/xderiv2)
             x_actual=xsig
             #print("{} - {}".format(x_actual,(xderiv/xderiv2) ))
+            cont+=1
             
         return xsig
     
@@ -46,15 +55,16 @@ class optimizador:
     def busqueda_unidireccional(self,epsilon):
         alpha_nuevo=self.newton_raphson(self.alfa,epsilon,self.evaluar_alpha)
         x_alpha_new=(self.x_t) + (alpha_nuevo * self.x_s)
-        return self.funcion(x_alpha_new[0],x_alpha_new[1])
+        return (x_alpha_new[0],x_alpha_new[1])
 
 
 
-x_t=np.array([2,1])
-x_s=np.array([2,5])
+x_t=np.array([1,1])
+x_s=np.array([1,0.5])
 alpha=0.5
+
 epsilon=0.1
-optimi=optimizador(x_t,x_s,funcion1,alpha)
+optimi=optimizador(x_t,x_s,himmelblau,alpha)
 print(optimi.busqueda_unidireccional(epsilon))
 
 
