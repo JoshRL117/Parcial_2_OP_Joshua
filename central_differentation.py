724dbb78ba876afe1c1348384440af33085ca816
JoshRL117 <100550089+JoshRL117@users.noreply.github.com>
2024-06-03 09:51:15 -0600

    N_dimensions_update

--- central_differentation.py
+++ central_differentation.py
@@ -9,7 +9,11 @@ def gradiente_calculation(x,delta,f):
             point=np.array(x_work_f,copy=True)
             vector_f1_prim.append(primeraderivadaop(point,i,delta,f))
         return vector_f1_prim
-            
+    else:
+        for i in range(len(x_work_f)):
+            point=np.array(x_work_f,copy=True)
+            vector_f1_prim.append(primeraderivadaop(point,i,delta[i],f))
+        return vector_f1_prim
 
 def primeraderivadaop(x,i,delta,f):
     mof=x[i]
@@ -39,7 +43,7 @@ def derivadadodadoop(x,index_principal,index_secundario,delta,f):
     p2=np.array(x,copy=True)
     p3=np.array(x,copy=True)
     p4=np.array(x,copy=True)
-    if isinstance(delta,int) or isinstance(delta,float):
+    if isinstance(delta,int) or isinstance(delta,float):#Cuando delta es un solo valor y no un arreglo 
         mod1=mof + delta
         mod2=mof - delta
         mod3=mof2 + delta
@@ -54,24 +58,35 @@ def derivadadodadoop(x,index_principal,index_secundario,delta,f):
         p4[index_secundario]=mod4
         numerador=((f(p)) - f(p2) - f(p3) + f(p4))
         return numerador / (4*delta)
+    else:#delta si es un arreglo 
+        mod1=mof + delta[index_principal]
+        mod2=mof - delta[index_principal]
+        mod3=mof2 + delta[index_secundario]
+        mod4=mof2 - delta[index_secundario]
+        p[index_principal]=mod1
+        p[index_secundario]=mod3
+        p2[index_principal]=mod1
+        p2[index_secundario]=mod4
+        p3[index_principal]=mod2
+        p3[index_secundario]=mod3
+        p4[index_principal]=mod2
+        p4[index_secundario]=mod4
+        numerador=((f(p)) - f(p2) - f(p3) + f(p4))
+        return numerador / (4*delta)
+
     
-def hessian_matrix(x,delt,f):# x es el vector 
+def hessian_matrix(x,delt,f):# x es el vector de variables
     matrix_f2_prim=[([0]*len(x)) for i in range(len(x))]
     x_work=np.array(x)
     x_work_f=x_work.astype(np.float64)
-    if isinstance(delt,int) or isinstance(delt,float):
-        for i in range(len(x)):
-            point=np.array(x_work_f,copy=True)
-            for j in range(len(x)):
-                if i == j:
-                    matrix_f2_prim[i][j]=segundaderivadaop(point,i,delt,f)
-                else:
-                    matrix_f2_prim[i][j]=derivadadodadoop(point,i,j,delt,f)
+    for i in range(len(x)):
+        point=np.array(x_work_f,copy=True)
+        for j in range(len(x)):
+            if i == j:
+                matrix_f2_prim[i][j]=segundaderivadaop(point,i,delt,f)
+            else:
+                matrix_f2_prim[i][j]=derivadadodadoop(point,i,j,delt,f)
     return matrix_f2_prim
-
-
-
-
 
 def himmelblau(p):
     return (p[0]**2 + p[1] - 11)**2 + (p[0] + p[1]**2 - 7)**2
